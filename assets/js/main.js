ea36bdf73b3d69677de555d70adff39598280b0e
aaron-nls <aaron@northlainesolutions.com>
2018-08-10 19:52:10 +0100

    adding classes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,11 @@ document.addEventListener("DOMContentLoaded",function(){
     topStories.then(feed => {
         /* TODO: feed.stories as an array of story objects and function move into render method */
         /* Wait for all story promises to resolves */
-        Promise.all(feed.stories).then(function(stories){ let html ='<ol class="list">';
+        Promise.all(feed.stories).then(function(stories){ 
+            let html ='<ol class="list">';
             for (let i in stories) {
                 let date = new Date(stories[i].time * 1000);
-                html += '<li itemscope itemtype="http://schema.org/Article"><h2 itemprop="name"><a href="'+stories[i].url+'" target="_blank">'+stories[i].title+'</a></h2><div><span itemprop="author">By: '+stories[i].by+'</span><span datetime="'+ date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate() +'"  itemprop="datePublished">Posted: '+ date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear() +'</span></div></li>';
+                html += '<li class="list__item story" itemscope itemtype="http://schema.org/Article"><h2 class="story__title" itemprop="name"><a href="'+stories[i].url+'" target="_blank">'+stories[i].title+'</a></h2><div class="story__summary"><span itemprop="author">By: '+stories[i].by+'</span><span datetime="'+ date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate() +'"  itemprop="datePublished">Posted: '+ date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear() +'</span></div></li>';
             }
             document.getElementById('newsfeed').innerHTML = html + '</ol>';
             document.querySelector('body').classList.remove('loading');
