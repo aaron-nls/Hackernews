3dc68c6be55da93d6408e21132a16930d1c94a00
aaron-nls <aaron@northlainesolutions.com>
2018-08-10 19:23:06 +0100

    getting feed of stories

--- assets/js/News.js
+++ assets/js/News.js
@@ -1,6 +1,23 @@
 class News {
-    constructor() {
+    constructor(feed) {
+        this.stories = feed;
     }
-  }
+
+    static load(url) {
+        return fetch(url)
+            .then(response => response.json())
+            .then(feed => new this(feed));
+    }
+
+    //Setter
+    set stories(feed) {  
+        this._stories=feed;
+    }
+
+    // Getter
+    get stories() {  
+        return this._stories;
+    }
+}
   
 export default News;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 import News from './News.js';
 
 document.addEventListener("DOMContentLoaded",function(){
-
+    let topStories = News.load('https://hacker-news.firebaseio.com/v0/topstories.json');
+    /* Wait for stories to load */
+    topStories.then(feed => {
+        console.log(feed.stories);
+    });
 });
